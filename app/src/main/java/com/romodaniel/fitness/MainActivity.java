117ce6a82d4c8bc845a379b1f09ebfa647cbc5b4
DanielrR0m0 <drdanielromo@gmail.com>
2017-07-31 16:02:58 -0700

    fixed transitions

--- app/src/main/java/com/romodaniel/fitness/MainActivity.java
+++ app/src/main/java/com/romodaniel/fitness/MainActivity.java
@@ -108,7 +108,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
         if(isFirst){
-          //  findViewById(R.id.user_first).setVisibility(View.VISIBLE);
+          //findViewById(R.id.user_first).setVisibility(View.VISIBLE);
 
             submit = (Button)  findViewById(R.id.submit);
             fName = (EditText) findViewById(R.id.f_name);
